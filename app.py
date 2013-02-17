d243729ab09cd8c2888eb7789eaa32b0472e9344
acm46 <anirudh.c.mohan@gmail.com>
2013-02-16 21:43:18 -0500

    Refactoring App

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'citiesapp'
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def home_page():
+	online_users = tojson(mongo.db.users.find()[0])
+	print online_users.name
+	return '<p>online_users</p>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
